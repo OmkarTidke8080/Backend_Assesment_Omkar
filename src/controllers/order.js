833339811a32ff07499f7926c9191e3175812c9c
OmkarTidke8080 <omkartidke8080@gmail.com>
2024-12-24 10:34:46 +0530

    Added discount logic

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -1,39 +1,74 @@
 let orders = [];
 
-//  New Order API
+// Create  Order
 export const order = (req, res) => {
-  const { product_Name, quantity, price } = req.body;
+  const { products } = req.body;
 
-  if (!product_Name || !quantity || !price) {
-    return res.status(400).json({ error: "All fields are required" });
+  if (!products || !Array.isArray(products) || products.length === 0) {
+    return res
+      .status(400)
+      .json({ error: "Products are required and must be an array" });
   }
 
+  let totalAmount = 0;
+  let discount = 0;
+
+  products.forEach((product) => {
+    if (!product.product_Name || !product.quantity || !product.price) {
+      return res.status(400).json({
+        error: "Each product must have a product_Name, quantity, and price",
+      });
+    }
+
+    totalAmount += product.quantity * product.price;
+  });
+
+  // discount based on price exceeding 10000
+  if (totalAmount > 10000) {
+    discount += totalAmount * 0.1;
+  }
+
+  // discount based on more than 5 items
+  if (products.length > 5) {
+    discount += 500;
+  }
+
+  const finalAmount = totalAmount - discount;
+
+  // Create an order object
   const order = {
     id: orders.length + 1,
-    product_Name,
-    quantity,
-    price,
-    total: quantity * price,
+    products,
+    totalAmount,
+    discount,
+    finalAmount,
   };
 
   orders.push(order);
+
   res.status(201).json(order);
 };
 
-// get All Orders
+// Get All Orders
 export const getAllOrders = (req, res) => {
-  res.status(200).json(orders);
+  if (orders.length > 0) {
+    res.status(200).json(orders);
+  } else {
+    res.status(200).json({
+      message: "No orders available.",
+    });
+  }
 };
-
-
 
 // get total revenue
 export const totalRevenue = (req, res) => {
   let sum = 0;
 
-  for (let i = 0; i < orders.length; i++) {
-    sum += orders[i].price * orders[i].quantity; 
-  }
+  orders.forEach((order) => {
+    order.products.forEach((product) => {
+      sum += product.price * product.quantity;
+    });
+  });
 
   res.status(200).json({ totalRevenue: sum });
 };
